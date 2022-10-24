fa7ef2ede2dec6e1a3057e5b57409df01d875dd1
jastineee <201810083@fit.edu.ph>
2022-10-24 19:29:28 +0800

    change name on the home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,13 +1,16 @@
 <?php
-class Home extends CI_Controller {
-    public function __construct(){
+class Home extends CI_Controller
+{
+    public function __construct()
+    {
         parent::__construct();
         $this->load->helper('url');
     }
 
-    public function index() {
+    public function index()
+    {
         // Create an array to be passed to the view
-        $data['full_name'] = 'Joseph Calleja';
+        $data['full_name'] = 'Guillermo, Justine Rome M.';
         $data['title'] = "Kuya Jay's Karinderia";
         $this->load->view('include/header', $data);
         $this->load->view('include/navbar');
@@ -15,7 +18,8 @@ class Home extends CI_Controller {
         $this->load->view('include/footer');
     }
 
-    public function view_products() {
+    public function view_products()
+    {
         $this->load->model('Products_model');
         $data['products'] = $this->Products_model->get_products_no_limit();
         $data['title'] = 'View Products';
@@ -24,5 +28,4 @@ class Home extends CI_Controller {
         $this->load->view('product_view', $data);
         $this->load->view('include/footer');
     }
-    
 }
